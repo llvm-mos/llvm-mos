496fe32934560b4e51a91d3c6b543feda430e01e
Daniel Thornburgh <mysterymath@gmail.com>
2021-03-15 17:22:57 -0700

    Revert "Attach MOS to the BareMetal clang toolchain."
    This reverts commit d8de18fe898b6ea208dd375c541fd340b65341e1.

--- clang/lib/Driver/ToolChains/BareMetal.cpp
+++ clang/lib/Driver/ToolChains/BareMetal.cpp
@@ -125,10 +125,6 @@ static bool isARMBareMetal(const llvm::Triple &Triple) {
   return true;
 }
 
-static bool isMOSBareMetal(const llvm::Triple &Triple) {
-  return Triple.getArch() == llvm::Triple::mos;
-}
-
 static bool isRISCVBareMetal(const llvm::Triple &Triple) {
   if (Triple.getArch() != llvm::Triple::riscv32 &&
       Triple.getArch() != llvm::Triple::riscv64)
@@ -155,7 +151,7 @@ void BareMetal::findMultilibs(const Driver &D, const llvm::Triple &Triple,
 }
 
 bool BareMetal::handlesTarget(const llvm::Triple &Triple) {
-  return isARMBareMetal(Triple) || isMOSBareMetal(Triple) || isRISCVBareMetal(Triple);
+  return isARMBareMetal(Triple) || isRISCVBareMetal(Triple);
 }
 
 Tool *BareMetal::buildLinker() const {
