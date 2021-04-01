95c699efefd41de9c5cac75b69ec1ec0571fd3f1
Daniel Thornburgh <mysterymath@gmail.com>
2021-04-01 00:08:04 -0700

    [NFC] Add headers to isel.

--- llvm/lib/Target/MOS/MOSInstructionSelector.cpp
+++ llvm/lib/Target/MOS/MOSInstructionSelector.cpp
@@ -1,3 +1,15 @@
+//===-- MOSInstructionSelector.cpp - MOS Instruction Selector -------------===//
+//
+// Part of LLVM-MOS, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file defines the MOS instruction selector.
+//
+//===----------------------------------------------------------------------===//
+
 #include "MOSInstructionSelector.h"
 
 #include <set>
--- llvm/lib/Target/MOS/MOSInstructionSelector.h
+++ llvm/lib/Target/MOS/MOSInstructionSelector.h
@@ -1,3 +1,15 @@
+//===-- MOSInstructionSelector.h - MOS Instruction Selector -----*- C++ -*-===//
+//
+// Part of LLVM-MOS, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file declares the MOS instruction selector.
+//
+//===----------------------------------------------------------------------===//
+
 #ifndef LLVM_LIB_TARGET_MOS_MOSINSTRUCTIONSELECTOR_H
 #define LLVM_LIB_TARGET_MOS_MOSINSTRUCTIONSELECTOR_H
 
