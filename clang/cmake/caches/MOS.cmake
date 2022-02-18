3b08cae37e122499e8f23069a2981549e775263a
Daniel Thornburgh <mysterymath@gmail.com>
2022-02-17 19:17:53 -0800

    Set default target to MOS; disable X86.

--- clang/cmake/caches/MOS.cmake
+++ clang/cmake/caches/MOS.cmake
@@ -3,44 +3,46 @@
 # Usage for configuring:
 #   cmake -C [path-to-this-file] ...
 
-set(LLVM_TARGETS_TO_BUILD "X86" 
+set(LLVM_TARGETS_TO_BUILD ""
     CACHE STRING "LLVM targets to build")
-set(LLVM_EXPERIMENTAL_TARGETS_TO_BUILD "MOS" 
+set(LLVM_EXPERIMENTAL_TARGETS_TO_BUILD "MOS"
     CACHE STRING "LLVM experimental targets to build")
 set(LLVM_ENABLE_PROJECTS clang;clang-tools-extra;lld
     CACHE STRING "LLVM projects to enable")
-set(LIBXML2_LIBRARIES "IGNORE" 
+set(LIBXML2_LIBRARIES "IGNORE"
     CACHE STRING "Ignore LibXML2 libraries")
-set(LLVM_INSTALL_TOOLCHAIN_ONLY ON 
+set(LLVM_INSTALL_TOOLCHAIN_ONLY ON
     CACHE BOOL "LLVM install toolchain only")
+
+set(LLVM_DEFAULT_TARGET_TRIPLE "mos" CACHE STRING "")
 
 # The following options are principally to reduce space on Github action
 # runner builds. They make smaller, and possibly slower, releases; but the
-# releases are already over 1GB without them on most platforms, and the 
+# releases are already over 1GB without them on most platforms, and the
 # compilers don't seem to be slow on MOS-sized projects.  If you have
 # more disk space, you may not need them.
 
-set(CMAKE_BUILD_TYPE MinSizeRel 
+set(CMAKE_BUILD_TYPE MinSizeRel
     CACHE STRING "CMake build type")
 if(LLVM_MOS_BUILD_DYNAMIC_LIBRARIES)
-    set(LLVM_BUILD_LLVM_DYLIB ON 
+    set(LLVM_BUILD_LLVM_DYLIB ON
         CACHE BOOL "Build LLVM dynamic libraries")
-    set(LLVM_LINK_LLVM_DYLIB ON 
+    set(LLVM_LINK_LLVM_DYLIB ON
         CACHE BOOL "Link LLVM dynamic libraries")
 endif(LLVM_MOS_BUILD_DYNAMIC_LIBRARIES)
-	
+
 # disable lldb testing until the lldb tests stabilize
-set(LLDB_INCLUDE_TESTS OFF 
+set(LLDB_INCLUDE_TESTS OFF
     CACHE BOOL "Include lldb tests")
-	
-# sccache or ccache may be used to speed up builds, if we can find it 
+
+# sccache or ccache may be used to speed up builds, if we can find it
 # on the path
 if(LLVM_MOS_USE_COMPILER_CACHE)
-find_program(LLVM_MOS_CCACHE 
+find_program(LLVM_MOS_CCACHE
     NAMES sccache ccache
     DOC "Path to compiler caching program")
 if (NOT LLVM_MOS_CCACHE STREQUAL "LLVM_MOS_CCACHE-NOTFOUND")
-    set(CMAKE_C_COMPILER_LAUNCHER ${LLVM_MOS_CCACHE} 
+    set(CMAKE_C_COMPILER_LAUNCHER ${LLVM_MOS_CCACHE}
         CACHE PATH "Path to C compiler caching program")
     set(CMAKE_CXX_COMPILER_LAUNCHER ${LLVM_MOS_CCACHE}
         CACHE PATH "Path to C++ compiler caching program")
@@ -82,7 +84,7 @@ set(LLVM_MOS_TOOLCHAIN_TOOLS
     llvm-strip
     opt
 )
-set(LLVM_TOOLCHAIN_TOOLS ${LLVM_MOS_TOOLCHAIN_TOOLS} 
+set(LLVM_TOOLCHAIN_TOOLS ${LLVM_MOS_TOOLCHAIN_TOOLS}
     CACHE STRING "LLVM toolchain tools")
 
 # Add clang symlinks prefixed with mos-* which will implicitly add --target=mos when executed.
