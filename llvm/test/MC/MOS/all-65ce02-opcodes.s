70c694c918d97c8f408f9d1f3d2ea7b7543eb803
Mikael Lund <Mlund@mac.com>
2022-06-27 19:00:48 +0100

    Add more remaining opcodes to test

--- llvm/test/MC/MOS/all-65ce02-opcodes.s
+++ llvm/test/MC/MOS/all-65ce02-opcodes.s
@@ -1,15 +1,19 @@
 ; RUN: llvm-mc -assemble --print-imm-hex --show-encoding -triple mos --mcpu=mosw65ce02 < %s | FileCheck %s
 
-	neg                         ; CHECK: encoding: [0x42]
-	asr                         ; CHECK: encoding: [0x43]
 	inz                         ; CHECK: encoding: [0x1b]
 	dez                         ; CHECK: encoding: [0x3b]
+	neg                         ; CHECK: encoding: [0x42]
+	asr                         ; CHECK: encoding: [0x43]
 	taz                         ; CHECK: encoding: [0x4b]
 	tza                         ; CHECK: encoding: [0x6b]
 	phz                         ; CHECK: encoding: [0xdb]
 	plz                         ; CHECK: encoding: [0xfb]
 
-        ldz     #$ea                    ; CHECK: encoding: [0xa3,0xea]
- 	ldz	$eaea                   ; CHECK: encoding: [0xab,0xea,0xea]
- 	ldz	$eaea,x                 ; CHECK: encoding: [0xbb,0xea,0xea]
+        rtn     #$ea                ; CHECK: encoding: [0x62,0xea]
+        dew     $ea                 ; CHECK: encoding: [0xc3,0xea]
+        inw     $ea                 ; CHECK: encoding: [0xe3,0xea]
+
+        ldz     #$ea                ; CHECK: encoding: [0xa3,0xea]
+ 	ldz	$eaea               ; CHECK: encoding: [0xab,0xea,0xea]
+ 	ldz	$eaea,x             ; CHECK: encoding: [0xbb,0xea,0xea]
 
