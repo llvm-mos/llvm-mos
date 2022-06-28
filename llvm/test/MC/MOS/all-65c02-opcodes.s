6dcdba38307201142c9f115e76bc21da1440bfd2
Mikael Lund <Mlund@mac.com>
2022-06-28 16:44:53 +0100

    Add 65c02 TRB

--- llvm/test/MC/MOS/all-65c02-opcodes.s
+++ llvm/test/MC/MOS/all-65c02-opcodes.s
@@ -17,6 +17,8 @@
  	bit	$ea,x                   ; CHECK: encoding: [0x34,0xea]
  	bit	$eaea,x                 ; CHECK: encoding: [0x3c,0xea,0xea]
  	bit	#$ea                    ; CHECK: encoding: [0x89,0xea]
+ 	trb	$ea                     ; CHECK: encoding: [0x14,0xea]
+ 	trb	$eaea                   ; CHECK: encoding: [0x1c,0xea,0xea]
 
  	jmp	($eaea,x)               ; CHECK: encoding: [0x7c,0xea,0xea]
 
