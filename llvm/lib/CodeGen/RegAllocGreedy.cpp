8573cdc3dc36c7853f141f0dcf8050a6000510f9
Daniel Thornburgh <mysterymath@gmail.com>
2023-03-03 18:29:05 -0800

    Allow rematerialization during light spilling.

--- llvm/lib/CodeGen/RegAllocGreedy.cpp
+++ llvm/lib/CodeGen/RegAllocGreedy.cpp
@@ -1331,16 +1331,6 @@ unsigned RAGreedy::tryInstructionSplit(const LiveInterval &VirtReg,
   // Always enable split spill mode, since we're effectively spilling to a
   // register.
   LiveRangeEdit LREdit(&VirtReg, NewVRegs, *MF, *LIS, VRM, this, &DeadRemats);
-  if (LightSpill) {
-    if (LREdit.anyRematerializable()) {
-      // Prefer to remat by spilling rather than inserting unnecessary copies.
-      ExtraInfo->setStage(LREdit.begin(), LREdit.end(), RS_Spill);
-      return 0;
-    }
-    // Don't rematerialize during the light-spill stage, as this can cause
-    // register classes to become over-constrained.
-    LREdit.setRematEnable(false);
-  }
   SE->reset(LREdit, SplitEditor::SM_Size);
 
   ArrayRef<SlotIndex> Uses = SA->getUseSlots();
