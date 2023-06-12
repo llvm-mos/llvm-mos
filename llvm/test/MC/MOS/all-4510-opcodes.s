2aa9e9003a770ea7834124d65b9f12328cf6ae02
Adrian Siekierka <kontakt@asie.pl>
2023-06-12 21:44:10 +0200

    Add EOM 4510 opcode alias.

--- llvm/test/MC/MOS/all-4510-opcodes.s
+++ llvm/test/MC/MOS/all-4510-opcodes.s
@@ -1,3 +1,4 @@
 ; RUN: llvm-mc -assemble --print-imm-hex --show-encoding -triple mos --mcpu=mos4510 < %s | FileCheck %s
 
 	map                             ; CHECK: encoding: [0x5c]
+	eom                             ; CHECK: encoding: [0xea]
