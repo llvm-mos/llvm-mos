8411c4e77ff6ca69c3147dac1ab4fa74b2fd0b15
Daniel Thornburgh <mysterymath@gmail.com>
2025-06-25 22:02:21 -0700

    Remove MOSIncDecPhi pass
    This pass isn't very well conceived. Issue #491 demonstrates an example,
    where, if it worked properly, it would move a load into a loop
    unnecessarily. I think this needs to be rewritten from the ground up,
    since as is, it's a very large amount of unusually brittle and complex
    code for a very small gain. Accordingly, I'm removing the pass while I
    focus on the register allocator.
    
    Fixes #491.

--- llvm/lib/Target/MOS/MOS.h
+++ llvm/lib/Target/MOS/MOS.h
@@ -21,7 +21,6 @@ namespace llvm {
 
 void initializeMOSCombinerPass(PassRegistry &);
 void initializeMOSCopyOptPass(PassRegistry &);
-void initializeMOSIncDecPhiPass(PassRegistry &);
 void initializeMOSIndexIVPass(PassRegistry &);
 void initializeMOSInsertCopiesPass(PassRegistry &);
 void initializeMOSInternalizePass(PassRegistry &);
